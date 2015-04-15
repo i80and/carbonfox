efb4d6bfc5df77156752ff3ade4766be13ab62d5
Andrew Aldridge <i80and@foxquill.com>
2015-04-15 17:29:59 -0400

    Give errors a toString method, and update the About layout

--- src/About.js
+++ src/About.js
@@ -4,20 +4,16 @@ let _ = document.webL10n.get
 
 export const view = function() {
     return m('div#view', [
-        m('div#title', _('%about')),
+        m('div#title', [
+            m('span.fa.fa-chevron-left.title-button.left', { onclick: () => history.back() }),
+            m('span', _('%about')),
+        ]),
         m('section#welcome-pane', [
             m('section.textblock', [
                 m('h1', 'Carbon Fox'),
                 m('h2', 'Details'),
                 DetailsComponent.getDetailsComponent()
-            ]),
-            m('div.vspacer'),
-            m('a.bb-button.recommend', {
-                onclick: () => {
-                    console.log('back')
-                    history.back()
-                }
-            }, _('%back'))
+            ])
         ])
     ])
 }
--- src/util.js
+++ src/util.js
@@ -28,7 +28,10 @@ export function error(name, message) {
     return {
         name: name,
         message: message,
-        stack: (new Error()).stack
+        stack: (new Error()).stack,
+        toString: function() {
+            return `Error: ${this.name}: ${this.message}\n${this.stack}`
+        }
     }
 }
 
